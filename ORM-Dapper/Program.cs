4d38ef525903a218e385a3291267a5b84235ceb1
Lisa Walters <lisacampiwalters@gmail.com>
2023-06-06 00:48:44 -0600

    set up dapper functionality

--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace ORM_Dapper
+﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace ORM_Dapper
 {
     public class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var config = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+            string connString = config.GetConnectionString("DefaultConnection");//grabbing info from connectionstring and storing it in connString
+            IDbConnection conn = new MySqlConnection(connString);
         }
     }
 }
