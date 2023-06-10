e614a59fc864abf5943596062837040152f5f8bf
Lisa Walters <lisacampiwalters@gmail.com>
2023-06-09 23:51:54 -0600

    added configuration and insert

--- ORM-Dapper/DapperDepartmentRepository.cs
+++ ORM-Dapper/DapperDepartmentRepository.cs
@@ -19,5 +19,16 @@ namespace ORM_Dapper
         {
             return _connection.Query<Department>("SELECT * FROM Departments;");
         }
+
+        public void InsertDepartment(string newDepartmentName)
+        {
+            _connection.Execute("INSERT INTO DEPARTMENTS (Name) VALUES (@departmentName);",
+              new { departmentName = newDepartmentName });
+        }
+
+        public void InsertDepartment()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -10,12 +10,40 @@ namespace ORM_Dapper
     {
         private static void Main(string[] args)
         {
+            #region Configuration
             var config = new ConfigurationBuilder()
                     .SetBasePath(Directory.GetCurrentDirectory())
                     .AddJsonFile("appsettings.json")
                     .Build();
             string connString = config.GetConnectionString("DefaultConnection");//grabbing info from connectionstring and storing it in connString
             IDbConnection conn = new MySqlConnection(connString);
-        }
-    }
-}
+
+            Console.WriteLine(connString);
+            Console.WriteLine("");
+            #endregion
+
+            var repo = new DapperDepartmentRepository(conn);
+            Console.WriteLine("Hello User! Here are the current departments: ");
+            Console.WriteLine("Please press enter...");
+            Console.ReadLine();
+            IEnumerable<Department> departments = repo.GetAllDepartments();
+
+            foreach (Department department in departments)
+            {
+                Console.WriteLine($"Department ID: {department.DepartmentID} Department Name: {department.Name}");
+            }
+
+            Console.WriteLine("Do you want to add a department?");
+            string userResponse = Console.ReadLine();
+            if (userResponse.ToLower() == "yes")
+            {
+                Console.WriteLine("What is the name of your new department?");
+                userResponse = Console.ReadLine();
+                repo.InsertDepartment(userResponse);
+            }
+            Console.WriteLine("Oh well. Goodbye!!!");
+
+
+        }//end main method
+    }//end program class
+}//end namespace
