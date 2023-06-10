8795fabce5c5cb1390263e48191377d225ef0d28
Lisa Walters <lisacampiwalters@gmail.com>
2023-06-10 15:14:19 -0600

    added friendly insert method

--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -23,16 +23,15 @@ namespace ORM_Dapper
             #endregion
 
             var repo = new DapperDepartmentRepository(conn);
-            Console.WriteLine("Hello User! Here are the current departments: ");
+            Console.WriteLine("Hello User! Here is the list of the current departments: ");
             Console.WriteLine("Please press enter...");
             Console.ReadLine();
-            IEnumerable<Department> departments = repo.GetAllDepartments();
 
-            foreach (Department department in departments)
-            {
-                Console.WriteLine($"Department ID: {department.DepartmentID} Department Name: {department.Name}");
-            }
+            var departments = repo.GetAllDepartments();
+            Print(repo.GetAllDepartments());
 
+
+            Console.WriteLine("");
             Console.WriteLine("Do you want to add a department?");
             string userResponse = Console.ReadLine();
             if (userResponse.ToLower() == "yes")
@@ -40,10 +39,27 @@ namespace ORM_Dapper
                 Console.WriteLine("What is the name of your new department?");
                 userResponse = Console.ReadLine();
                 repo.InsertDepartment(userResponse);
+                Console.WriteLine("");
+                Console.WriteLine("Here is an updated list with your added department: ");
+                Console.WriteLine("");
+                Print(repo.GetAllDepartments());
+                Console.WriteLine(" ");
+                Console.WriteLine("Thanks for inserting a department! Goodbye!");
             }
-            Console.WriteLine("Oh well. Goodbye!!!");
-
-
+            else
+            {
+                Console.WriteLine("");
+                Console.WriteLine("OK. No inserted department today. Goodbye!!!");
+            }
         }//end main method
+
+        private static void Print(IEnumerable<Department> departments)
+        {
+            foreach (Department department in departments)
+            {
+                Console.WriteLine($"Department ID: {department.DepartmentID} Department Name: {department.Name}");
+            }
+        }
+
     }//end program class
 }//end namespace
